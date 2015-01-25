6fc7e57c3b9778997274dcadf25f791c4f524fe9
Murali Ambekar <murali.ambekar@gmail.com>
2015-01-25 03:23:27 +0000

    Finish it really

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 SampleApp::Application.routes.draw do
   
   resources :users
+  resources :sessions, only: [:new, :create, :destroy] 
   root  'static_pages#home'  
   match '/signup'   => 'users#new',            via: 'get'
+  match '/signin'   => 'sessions#new',         via: 'get'
+  match '/signout'  => 'sessions#destroy',     via: 'delete'
   match '/help'     => 'static_pages#help',    via: 'get'   
   match '/about'    => 'static_pages#about',   via: 'get'
   match '/contact'  => 'static_pages#contact', via: 'get'   
